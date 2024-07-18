0d81cd7b3f82256346db5be45093616bddd2b114
Shobhan Biswas <61372335+shobhanbiswas11@users.noreply.github.com>
2024-07-18 12:37:00 +0530

    init python

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,11 +1,23 @@
+from dotenv import load_dotenv
 from fastapi import FastAPI
+from fastapi.responses import JsonResponse
+from lib.create_jd import create_job_description
+
+# Load environment variables from the .env file
+load_dotenv()
 
 app = FastAPI()
 
-@app.get("/")
-async def read_root():
-    return {"Hello": "Hi"}
+@app.get("/create-jd")
+async def generate_jd(request, *args, **kwargs):
+    if request.method == "POST":
+            data = request.POST.dict()
 
-@app.get("/items/{item_id}")
-async def read_item(item_id: int, q: str = None):
-    return {"item_id": item_id, "q": q}
+            data['user_name'] = request.headers.get("X-MS-CLIENT-PRINCIPAL-NAME")
+                        
+            result = await create_job_description(data)
+            # download_url = await create_and_upload_blob_file(result['message'], data)
+            
+            # result['url'] = f"{download_url}{SAS_TOKEN}"
+
+            return JsonResponse(result)
